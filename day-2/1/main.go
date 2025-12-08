5ce02ed3191ac2bb31ed09cf989767b76d80b277
Ignatij Gichevski <ignatij.gichevski@gmail.com>
2025-12-08 10:55:51 +0100

    day 2

---
+++ day-2/1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func isInvalidId(id int64) bool {
+	curr := strconv.FormatInt(id, 10)
+	if len(curr)%2 != 0 {
+		return false
+	}
+	half := len(curr) / 2
+	for i := 0; i < half; i++ {
+		if curr[i] != curr[half+i] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+	total := 0
+	for scanner.Scan() {
+		for idRange := range strings.SplitSeq(scanner.Text(), ",") {
+			idRangeParts := strings.Split(idRange, "-")
+			start, _ := strconv.Atoi(idRangeParts[0])
+			end, _ := strconv.Atoi(idRangeParts[1])
+			for i := start; i <= end; i++ {
+				if isInvalidId(int64(i)) {
+					total += i
+				}
+			}
+		}
+	}
+	fmt.Println(total)
+}
