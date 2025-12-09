716d20080a41116147663444d59d26b87374e569
Ignatij Gichevski <ignatij.gichevski@gmail.com>
2025-12-09 20:53:58 +0100

    day-4: part two

---
+++ day-4/2/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+	graph := make([][]string, 0)
+	i := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		graph = append(graph, make([]string, len(line)))
+		for j := 0; j < len(line); j++ {
+			graph[i][j] = string(line[j])
+		}
+		i++
+	}
+	count := 0
+	cycle := false
+	for {
+		cycle = false
+		for i := 0; i < len(graph); i++ {
+			for j := 0; j < len(graph[i]); j++ {
+				if graph[i][j] == "@" {
+					adjacent := 0
+					if i > 0 && graph[i-1][j] == "@" {
+						adjacent++
+					}
+					if i < len(graph)-1 && graph[i+1][j] == "@" {
+						adjacent++
+					}
+					if j > 0 && graph[i][j-1] == "@" {
+						adjacent++
+					}
+					if j < len(graph[i])-1 && graph[i][j+1] == "@" {
+						adjacent++
+					}
+					if i > 0 && j > 0 && graph[i-1][j-1] == "@" {
+						adjacent++
+					}
+					if i > 0 && j < len(graph[i])-1 && graph[i-1][j+1] == "@" {
+						adjacent++
+					}
+					if i < len(graph)-1 && j > 0 && graph[i+1][j-1] == "@" {
+						adjacent++
+					}
+					if i < len(graph)-1 && j < len(graph[i])-1 && graph[i+1][j+1] == "@" {
+						adjacent++
+					}
+					if adjacent < 4 {
+						count++
+						cycle = true
+						graph[i][j] = "."
+					}
+				}
+
+			}
+		}
+		if !cycle {
+			break
+		}
+	}
+
+	fmt.Println(count)
+}
