cae31289bac45e63ea87caf915c420ae80d8da35
Ignatij Gichevski <ignatij.gichevski@gmail.com>
2026-01-06 13:09:18 +0100

    day 9 part 2 one more time

--- day-9/2/main.go
+++ day-9/2/main.go
@@ -4,37 +4,48 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 )
 
 type Point struct {
-	X int
-	Y int
+	X, Y int
 }
 
-func unique(arr []int) []int {
-	seen := make(map[int]bool)
-	for _, v := range arr {
-		seen[v] = true
-	}
-	out := make([]int, 0, len(seen))
-	for v := range seen {
-		out = append(out, v)
-	}
-	slices.Sort(out)
-	return out
+type CompressedPoint struct {
+	X, Y   int
+	Xi, Yi int
 }
 
-func imin(a, b int) int {
+func sortInts(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+}
+
+func uniqueInts(arr []int) []int {
+	sortInts(arr)
+	result := make([]int, 0, len(arr))
+	for i := 0; i < len(arr); i++ {
+		if i == 0 || arr[i] != arr[i-1] {
+			result = append(result, arr[i])
+		}
+	}
+	return result
+}
+
+func iMin(a, b int) int {
 	if a < b {
 		return a
 	}
 	return b
 }
 
-func imax(a, b int) int {
+func iMax(a, b int) int {
 	if a > b {
 		return a
 	}
@@ -42,190 +53,200 @@ func imax(a, b int) int {
 }
 
 func main() {
-	file, _ := os.Open("input.txt")
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
-	points := []Point{}
+	points := make([]Point, 0)
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.Split(line, ",")
 		x, _ := strconv.Atoi(parts[0])
 		y, _ := strconv.Atoi(parts[1])
-		points = append(points, Point{X: x, Y: y})
+		point := Point{X: x, Y: y}
+		points = append(points, point)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error scanning file:", err)
+		return
 	}
 
-	// coordinate compression padding each red point with neighbors
-	xs := []int{}
-	ys := []int{}
-	for _, p := range points {
-		xs = append(xs, p.X, p.X-1, p.X+1)
-		ys = append(ys, p.Y, p.Y-1, p.Y+1)
-	}
-	xs = unique(xs)
-	ys = unique(ys)
+	xi := []int{}
+	yi := []int{}
 
-	xsMap := make(map[int]int, len(xs))
-	for i, v := range xs {
-		xsMap[v] = i
+	// add neighbors
+	for _, point := range points {
+		xi = append(xi, point.X, point.X-1, point.X+1)
+		yi = append(yi, point.Y, point.Y-1, point.Y+1)
 	}
-	ysMap := make(map[int]int, len(ys))
-	for i, v := range ys {
-		ysMap[v] = i
-	}
+	xi = uniqueInts(xi)
+	yi = uniqueInts(yi)
 
-	type CompressedPoint struct {
-		X, Y   int
-		Xi, Yi int
+	// compression
+	xsMap := make(map[int]int)
+	ysMap := make(map[int]int)
+	for i, x := range xi {
+		xsMap[x] = i
 	}
-	compressed := make([]CompressedPoint, len(points))
-	for i, p := range points {
-		compressed[i] = CompressedPoint{X: p.X, Y: p.Y, Xi: xsMap[p.X], Yi: ysMap[p.Y]}
+	for i, y := range yi {
+		ysMap[y] = i
 	}
 
-	w := len(xs) - 1
-	h := len(ys) - 1
-
-	hWall := make([][]bool, len(ys))
-	for i := range hWall {
-		hWall[i] = make([]bool, w)
+	compressedPoints := make([]CompressedPoint, len(points))
+	for i, point := range points {
+		compressedPoints[i] = CompressedPoint{
+			X:  point.X,
+			Y:  point.Y,
+			Xi: xsMap[point.X],
+			Yi: ysMap[point.Y],
+		}
 	}
-	vWall := make([][]bool, len(xs))
+
+	// build horizontall and vertical walls
+	w := len(xi) - 1
+	h := len(yi) - 1
+	vWall := make([][]bool, len(xi))
+	hWall := make([][]bool, len(yi))
+
 	for i := range vWall {
 		vWall[i] = make([]bool, h)
 	}
+	for i := range hWall {
+		hWall[i] = make([]bool, w)
+	}
 
-	for i := 0; i < len(points); i++ {
-		a := compressed[i]
-		b := compressed[(i+1)%len(points)]
-		// mark walls along polygon edges (axis-aligned)
+	for i := 0; i < len(compressedPoints); i++ {
+		a := compressedPoints[i]
+		b := compressedPoints[(i+1)%len(compressedPoints)]
 		if a.X == b.X {
-			x := a.Xi
-			y0 := imin(a.Yi, b.Yi)
-			y1 := imax(a.Yi, b.Yi)
+			y0 := iMin(a.Yi, b.Yi)
+			y1 := iMax(a.Yi, b.Yi)
 			for y := y0; y < y1; y++ {
-				vWall[x][y] = true
+				vWall[a.Xi][y] = true
 			}
 		}
 		if a.Y == b.Y {
-			y := a.Yi
-			x0 := imin(a.Xi, b.Xi)
-			x1 := imax(a.Xi, b.Xi)
+			x0 := iMin(a.Xi, b.Xi)
+			x1 := iMax(a.Xi, b.Xi)
 			for x := x0; x < x1; x++ {
-				hWall[y][x] = true
+				hWall[a.Yi][x] = true
 			}
 		}
 	}
 
+	// flood fill
 	outside := make([][]bool, w)
-	for i := 0; i < w; i++ {
+	for i := range outside {
 		outside[i] = make([]bool, h)
 	}
 
-	queue := make([][2]int, 0)
+	queue := [][2]int{}
 	push := func(x, y int) {
-		if x < 0 || x >= w || y < 0 || y >= h {
-			return
-		}
 		if outside[x][y] {
 			return
 		}
 		outside[x][y] = true
 		queue = append(queue, [2]int{x, y})
 	}
-
 	for i := 0; i < w; i++ {
 		push(i, 0)
-		if h > 1 {
-			push(i, h-1)
-		}
+		push(i, h-1)
 	}
-	for j := 0; j < h; j++ {
-		push(0, j)
-		if w > 1 {
-			push(w-1, j)
-		}
+	for i := 0; i < h; i++ {
+		push(0, i)
+		push(w-1, i)
 	}
 
 	for len(queue) > 0 {
-		cur := queue[0]
+		curr := queue[0]
 		queue = queue[1:]
-		x, y := cur[0], cur[1]
+		x, y := curr[0], curr[1]
+
 		if x > 0 && !vWall[x][y] {
 			push(x-1, y)
 		}
-		if x+1 < w && !vWall[x+1][y] {
+		if x < w-1 && !vWall[x+1][y] {
 			push(x+1, y)
 		}
 		if y > 0 && !hWall[y][x] {
 			push(x, y-1)
 		}
-		if y+1 < h && !hWall[y+1][x] {
+		if y < h-1 && !hWall[y+1][x] {
 			push(x, y+1)
 		}
+
 	}
 
-	for i := 0; i < len(points); i++ {
+	// walkway cleanup because of flood fill
+	for i := range points {
 		a := points[i]
 		b := points[(i+1)%len(points)]
-		// ensure walkway cells are treated as inside even if flood fill leaked
+
 		if a.X == b.X {
 			x := a.X
 			xi0 := xsMap[x]
 			xi1 := xsMap[x+1]
-			yi0 := ysMap[imin(a.Y, b.Y)]
-			yi1 := ysMap[imax(a.Y, b.Y)+1]
-			for xi := xi0; xi < xi1 && xi < w; xi++ {
-				for yi := yi0; yi < yi1 && yi < h; yi++ {
+			yi0 := ysMap[iMin(a.Y, b.Y)]
+			yi1 := ysMap[iMax(a.Y, b.Y)+1]
+
+			for xi := xi0; xi < xi1; xi++ {
+				for yi := yi0; yi < yi1; yi++ {
 					outside[xi][yi] = false
 				}
 			}
-		} else {
+		}
+		if a.Y == b.Y {
 			y := a.Y
-			xi0 := xsMap[imin(a.X, b.X)]
-			xi1 := xsMap[imax(a.X, b.X)+1]
 			yi0 := ysMap[y]
 			yi1 := ysMap[y+1]
-			for xi := xi0; xi < xi1 && xi < w; xi++ {
-				for yi := yi0; yi < yi1 && yi < h; yi++ {
+			xi0 := xsMap[iMin(a.X, b.X)]
+			xi1 := xsMap[iMax(a.X, b.X)+1]
+
+			for xi := xi0; xi < xi1; xi++ {
+				for yi := yi0; yi < yi1; yi++ {
 					outside[xi][yi] = false
 				}
 			}
 		}
 	}
 
-	// build prefix sums of "outside" cells for O(1) rectangle queries
+	// prefix calculation
 	prefix := make([][]int, w+1)
-	for i := 0; i <= w; i++ {
+	for i := range prefix {
 		prefix[i] = make([]int, h+1)
 	}
+
 	for i := 0; i < w; i++ {
 		for j := 0; j < h; j++ {
-			val := 0
+			s := 0
 			if outside[i][j] {
-				val = 1
+				s = 1
 			}
-			prefix[i+1][j+1] = prefix[i][j+1] + prefix[i+1][j] - prefix[i][j] + val
+			prefix[i+1][j+1] = prefix[i][j+1] + prefix[i+1][j] - prefix[i][j] + s
 		}
 	}
 
-	// brute-force every pair of red corners in original coordinates
-	maxArea := int64(0)
+	// calculate area max
+	maxArea := 0
 	for i := 0; i < len(points); i++ {
 		for j := i + 1; j < len(points); j++ {
-			x0 := imin(points[i].X, points[j].X)
-			x1 := imax(points[i].X, points[j].X)
-			y0 := imin(points[i].Y, points[j].Y)
-			y1 := imax(points[i].Y, points[j].Y)
+			x1 := iMin(points[i].X, points[j].X)
+			x2 := iMax(points[i].X, points[j].X)
+			y1 := iMin(points[i].Y, points[j].Y)
+			y2 := iMax(points[i].Y, points[j].Y)
 
-			xi0 := xsMap[x0]
-			xi1 := xsMap[x1+1]
-			yi0 := ysMap[y0]
-			yi1 := ysMap[y1+1]
+			x1i := xsMap[x1]
+			x2i := xsMap[x2+1]
+			y1i := ysMap[y1]
+			y2i := ysMap[y2+1]
 
-			count := prefix[xi1][yi1] - prefix[xi0][yi1] - prefix[xi1][yi0] + prefix[xi0][yi0]
+			count := prefix[x2i][y2i] - prefix[x2i][y1i] - prefix[x1i][y2i] + prefix[x1i][y1i]
 			if count == 0 {
-				width := int64(x1 - x0 + 1)
-				height := int64(y1 - y0 + 1)
-				area := width * height
+				area := (x2 - x1 + 1) * (y2 - y1 + 1)
 				if area > maxArea {
 					maxArea = area
 				}
@@ -234,4 +255,5 @@ func main() {
 	}
 
 	fmt.Println(maxArea)
+
 }
